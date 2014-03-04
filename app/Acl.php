5c18ae529b626d7bcda3bf0d7a764646a395d14c
Ben-Ho <benjamin.hohenwarter@gmail.com>
2014-03-04 20:16:56 +0100

    First commmit

---
+++ app/Acl.php
@@ -0,0 +1,11 @@
+<?php
+class Acl extends Kwf_Acl_Component
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->allow('admin', 'kwf_user_changeuser');
+        $this->allow('admin', 'kwf_user_users');
+
+    }
+}
