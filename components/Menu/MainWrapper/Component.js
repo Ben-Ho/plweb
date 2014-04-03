ff902bc16116cf146001092094924cdd761dc3ec
Ben-Ho <benjamin.hohenwarter@gmail.com>
2014-04-03 21:56:04 +0200

    Use simple js to open/close

--- components/Menu/MainWrapper/Component.js
+++ components/Menu/MainWrapper/Component.js
@@ -5,65 +5,73 @@ Kwf.onElementReady('.menuMainWrapper', function (el) {
         var marginLeft = el.getStyle('margin-left');
         return parseInt(marginLeft.substring(0, marginLeft.length-2));
     }
-
-    var dragging = false;
-    var startX = null;
-    var startMargin = getMargin();
-
-    var startFunction = function (posX) {
-        dragging = true;
-        startX = posX;
-        startMargin = getMargin();
-    };
-    var moveFunction = function (posX) {
-        if (dragging) {
-            var margin = Math.max(el.getWidth()*-1+dragRegionWidth, startMargin + posX - startX);
-            margin = Math.min(margin, 0);
-            el.setStyle('margin-left', margin+'px');
+    el.child('.dragRegion').on('click', function (ev) {
+        var margin = getMargin();
+        if (margin < 0) {
+            el.setStyle('margin-left', '0px');
+        } else {
+            el.setStyle('margin-left', -(el.getWidth() - dragRegionWidth)+'px');
         }
-    };
-    var endFunction = function () {
-        dragging = false;
-        startX = null;
-    };
-
-    el.child('.dragRegion').on('mousedown', function (ev) {
-        console.log('Mouse Down');
-        var mouseMoveFunction = function (ev) {
-            console.log('Mouse Move');
-            moveFunction(ev.xy[0]);
-        };
-        var mouseEndFunction = function (ev) {
-            console.log('Mouse End');
-            endFunction();
-            Ext.getBody().un('mouseup', mouseEndFunction);
-            Ext.getBody().un('mousemove', mouseMoveFunction);
-        };
-
-        startFunction(ev.xy[0]);
-
-        Ext.getBody().on('mouseup', mouseEndFunction);
-        Ext.getBody().on('mousemove', mouseMoveFunction);
     });
 
-    el.child('.dragRegion').on('touchstart', function (ev) {
-        console.log('Touch Start');
-        var touchMoveFunction = function (ev) {
-            console.log('Touch Move');
-            moveFunction(ev.browserEvent.touches[0].pageX);
-        };
-        var touchEndFunction = function (ev) {
-            console.log('Touch End');
-            endFunction();
-            Ext.getBody().un('touchend', touchEndFunction);
-            Ext.getBody().un('touchmove', touchMoveFunction);
-        };
+//    var dragging = false;
+//    var startX = null;
+//    var startMargin = getMargin();
 
-        console.log(ev.browserEvent);
-        console.log(ev.browserEvent.touches[0]);
-        startFunction(ev.browserEvent.touches[0].pageX);
+//    var startFunction = function (posX) {
+//        dragging = true;
+//        startX = posX;
+//        startMargin = getMargin();
+//    };
+//    var moveFunction = function (posX) {
+//        if (dragging) {
+//            var margin = Math.max(el.getWidth()*-1+dragRegionWidth, startMargin + posX - startX);
+//            margin = Math.min(margin, 0);
+//            el.setStyle('margin-left', margin+'px');
+//        }
+//    };
+//    var endFunction = function () {
+//        dragging = false;
+//        startX = null;
+//    };
 
-        Ext.getBody().on('touchend', touchEndFunction);
-        Ext.getBody().on('touchmove', touchMoveFunction);
-    });
+//    el.child('.dragRegion').on('mousedown', function (ev) {
+//        console.log('Mouse Down');
+//        var mouseMoveFunction = function (ev) {
+//            console.log('Mouse Move');
+//            moveFunction(ev.xy[0]);
+//        };
+//        var mouseEndFunction = function (ev) {
+//            console.log('Mouse End');
+//            endFunction();
+//            Ext.getBody().un('mouseup', mouseEndFunction);
+//            Ext.getBody().un('mousemove', mouseMoveFunction);
+//        };
+//
+//        startFunction(ev.xy[0]);
+//
+//        Ext.getBody().on('mouseup', mouseEndFunction);
+//        Ext.getBody().on('mousemove', mouseMoveFunction);
+//    });
+
+//    el.child('.dragRegion').on('touchstart', function (ev) {
+//        console.log('Touch Start');
+//        var touchMoveFunction = function (ev) {
+//            console.log('Touch Move');
+//            moveFunction(ev.browserEvent.touches[0].pageX);
+//        };
+//        var touchEndFunction = function (ev) {
+//            console.log('Touch End');
+//            endFunction();
+//            Ext.getBody().un('touchend', touchEndFunction);
+//            Ext.getBody().un('touchmove', touchMoveFunction);
+//        };
+//
+//        console.log(ev.browserEvent);
+//        console.log(ev.browserEvent.touches[0]);
+//        startFunction(ev.browserEvent.touches[0].pageX);
+//
+//        Ext.getBody().on('touchend', touchEndFunction);
+//        Ext.getBody().on('touchmove', touchMoveFunction);
+//    });
 });
