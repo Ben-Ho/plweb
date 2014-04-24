63d9aa271d369d0f44a4de3c169e4ed71d3cabcd
Ben-Ho <benjamin.hohenwarter@gmail.com>
2014-04-25 00:01:22 +0200

    Verwende Width um Shopping-list zu verstecken

--- components/ShoppingList/Component.js
+++ components/ShoppingList/Component.js
@@ -40,34 +40,69 @@ Kwf.onElementReady('.shoppingList', function (el) {
     }
 });
 
+////Hide shoppingList via margin. Aber irgendwie wird dann die Seite größer als der Bidlschirm.
+//Kwf.onElementReady('.shoppingList', function (el) {
+//    var dragRegionWidth = 20;
+//    el.setStyle('margin-right', -(el.getWidth()-dragRegionWidth)+'px');
+//    var getMargin = function () {
+//        var marginRight = el.getStyle('margin-right');
+//        return parseInt(marginRight.substring(0, marginRight.length-2));
+//    }
+//    el.on('click', function (ev) {
+//        var margin = getMargin();
+//        if (margin < 0) {
+//            el.setStyle('margin-right', '0px');
+//        } else {
+//            el.setStyle('margin-right', -(el.getWidth() - dragRegionWidth)+'px');
+//            var listEl = el.child('.list');
+//            listEl.select('.product').each(function (el) {
+//                if (el.child('.checkbox').dom.checked) {
+//                    var productId = el.dom.getAttribute('data-product-id');
+//                    var list = window.localStorage.getItem('shoppingList').split(';');
+//                    var newList = new Array();
+//                    for(var i = 0; i < list.length; i++) {
+//                        if (list[i] != productId) {
+//                            newList.push(list[i]);
+//                        }
+//                    }
+//                    window.localStorage.setItem('shoppingList', newList.join(';'));
+//                    el.remove();
+//                }
+//            });
+//        }
+//    });
+//});
+
+// Hide shoppingList via WIDTH
 Kwf.onElementReady('.shoppingList', function (el) {
     var dragRegionWidth = 20;
-    el.setStyle('margin-right', -(el.getWidth()-dragRegionWidth)+'px');
-    var getMargin = function () {
-        var marginRight = el.getStyle('margin-right');
-        return parseInt(marginRight.substring(0, marginRight.length-2));
-    }
-    el.on('click', function (ev) {
-        var margin = getMargin();
-        if (margin < 0) {
-            el.setStyle('margin-right', '0px');
-        } else {
-            el.setStyle('margin-right', -(el.getWidth() - dragRegionWidth)+'px');
-            var listEl = el.child('.list');
-            listEl.select('.product').each(function (el) {
-                if (el.child('.checkbox').dom.checked) {
-                    var productId = el.dom.getAttribute('data-product-id');
-                    var list = window.localStorage.getItem('shoppingList').split(';');
-                    var newList = new Array();
-                    for(var i = 0; i < list.length; i++) {
-                        if (list[i] != productId) {
-                            newList.push(list[i]);
-                        }
+    var shoppingListWidth = el.getWidth();
+    el.setWidth(dragRegionWidth);
+
+    var clearCheckedShoppingItems = function () {
+        var listEl = el.child('.list');
+        listEl.select('.product').each(function (el) {
+            if (el.child('.checkbox').dom.checked) {
+                var productId = el.dom.getAttribute('data-product-id');
+                var list = window.localStorage.getItem('shoppingList').split(';');
+                var newList = new Array();
+                for(var i = 0; i < list.length; i++) {
+                    if (list[i] != productId) {
+                        newList.push(list[i]);
                     }
-                    window.localStorage.setItem('shoppingList', newList.join(';'));
-                    el.remove();
                 }
-            });
+                window.localStorage.setItem('shoppingList', newList.join(';'));
+                el.remove();
+            }
+        });
+    };
+    el.on('click', function (ev) {
+        var width = el.getWidth()
+        if (width <= dragRegionWidth) {
+            el.setWidth(shoppingListWidth);
+        } else {
+            el.setWidth(dragRegionWidth);
+            clearCheckedShoppingItems();
         }
     });
 });
